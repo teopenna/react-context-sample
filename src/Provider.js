5c5d25caf9ad9d6f87cc84b6aa5d48deb9012578
Matteo Penna <penna.matteo@gmail.com>
2018-05-20 23:08:32 +0200

    Removing redux and adding Provider

---
+++ src/Provider.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Char from './hero';
+
+const DEFAULT_STATE = {
+    allChar: Char,
+    searchTerm: ''
+};
+
+export const ThemeContext = React.createContext(DEFAULT_STATE);
+
+export default class Provider extends Component {
+    state: DEFAULT_STATE;
+
+    searchTermChanged = searchTerm => {
+        this.setState({searchTerm});
+    };
+
+    render() {
+        return (
+            <ThemeContext.Provider
+                value={{
+                    ...this.state,
+                    searchTermChanged: this.searchTermChanged
+                }}
+            >
+                {this.props.children}
+            </ThemeContext.Provider>
+        );
+    }
+}
